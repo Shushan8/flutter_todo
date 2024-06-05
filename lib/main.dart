c59579352422de1b0346caf161d9cd14a2226642
unknown <tigran.hayrapetyan17ht@gmail.com>
2024-06-05 18:41:50 +0400

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_todo/onbording.dart';
 
 import 'business_logic/blocs/bloc_exports.dart';
-import 'data/models/task.dart';
 
 void main() {
   BlocOverrides.runZoned(() => runApp(MyApp()));
@@ -12,8 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) =>
-          TasksBloc(),
+      create: (context) => TasksBloc(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
