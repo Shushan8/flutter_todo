d59bb860a64eaaba63d9054113a16c5cc568b43c
Tigran <tigran.hayrapetyan17ht@gmail.com>
2024-07-16 12:27:41 +0400

    runed in android

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_todo/di/injectable.dart';
 import 'package:flutter_todo/onbording.dart';
-
 
 void main() {
   configureDependencies();
@@ -12,14 +12,12 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return 
-       MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.deepOrange,
-        ),
-        home: Onbording(),
-      );
-   
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepOrange,
+      ),
+      home: Onbording(),
+    );
   }
 }
